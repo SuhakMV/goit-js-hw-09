077ffdcf80ac60ac7f1150fa70a89794a2b17ba2
SuhakMV <103995334+SuhakMV@users.noreply.github.com>
2022-09-09 17:02:17 +0000

    Deploying to gh-pages from @ SuhakMV/goit-js-hw-09@a22ab4ec279b40108e94b795fb51520d7e81ab57 🚀

---
+++ 01-color-switcher.875499bb.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]").addEventListener("click",(()=>{document.body.setAttribute("style","background:")}));
+//# sourceMappingURL=01-color-switcher.875499bb.js.map
---
+++ 01-color-switcher.9e67782c.js
@@ -0,0 +1,2 @@
+document.querySelector("[data-start]").addEventListener("click",(function(){document.body.setAttribute("style","background:")}));
+//# sourceMappingURL=01-color-switcher.9e67782c.js.map
