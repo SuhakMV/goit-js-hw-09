a22ab4ec279b40108e94b795fb51520d7e81ab57
SuhakMV <sugamish@gmail.com>
2022-09-09 20:01:53 +0300

    09092001

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,13 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+  
+  const button = document.querySelector('[data-start]');
+  //const span = document.querySelector(".color");
+  
+  button.addEventListener("click", () => {
+    //span.textContent = getRandomHexColor();
+    document.body.setAttribute(
+      'style',
+      'background:' //+ span.textContent
+    );});
