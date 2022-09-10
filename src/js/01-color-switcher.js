64120f8cd816a26c32dd520cc7f54a41f9da67e1
SuhakMV <sugamish@gmail.com>
2022-09-10 12:16:28 +0300

    10091216

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,13 +1,27 @@
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   };
-  
-  const button = document.querySelector('[data-start]');
-  //const span = document.querySelector(".color");
-  
-  button.addEventListener("click", () => {
-    //span.textContent = getRandomHexColor();
-    document.body.setAttribute(
-      'style',
-      'background:' //+ span.textContent
-    );});
+  /**Selectors */
+  const buttonStart = document.querySelector('[data-start]');
+  const buttonStop = document.querySelector('[data-stop]');
+  const body = document.querySelector('body');
+
+  let timerId = null;
+  /**Listener */
+  buttonStart.addEventListener('click', onclickButtonStart);
+  buttonStop.addEventListener('click', onclickButtonStop);
+
+  /**Function start/stop change color */
+  function onclickButtonStart() {
+    buttonStart.disabled = true;
+    buttonStop.disabled = false;
+    timerId = setInterval(() => {
+      body.style.background = getRandomHexColor();
+    }, 1000);
+  }
+
+  function onclickButtonStop() {
+    buttonStart.disabled = false;
+    buttonStop.disabled = true;
+    clearInterval(timerId);
+  }
