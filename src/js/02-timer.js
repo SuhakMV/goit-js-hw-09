df65d69c6610623f5cc3a915951c881405b26dd2
SuhakMV <sugamish@gmail.com>
2022-09-12 21:22:34 +0300

    12092122

---
+++ src/js/02-timer.js
@@ -0,0 +1,19 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const inputFp = document.querySelector('[datetime-picker]');
+const btnStart = document.querySelector('[data-start]');
+const valueDays = document.querySelector('[data-days]');
+const valueHr = document.querySelector('[data-hours]');
+const valueMin = document.querySelector('[data-minutes]');
+const valueSec = document.querySelector('[data-seconds]');
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
