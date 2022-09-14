99c372d8530999aca47338ac110b4e550e5ff2d5
SuhakMV <sugamish@gmail.com>
2022-09-14 20:26:11 +0300

    14092026

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+import Notiflix from "notiflix";
+
+const firstDelay = document.querySelector('[name="delay"]');
+const delayStep = document.querySelector('[name="step"]');
+const amout = document.querySelector('[name="amount"]');
+const buttonCreate = document.querySelector('[type="submit"]');
+console.log(buttonCreate);
+
+let evtDelay = Number(firstDelay.value);
+let evtStep = Number(delayStep.value);
+let evtAmout = Number(amout.value);
+
+buttonCreate.addEventListener('click', onInterval);
+
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
